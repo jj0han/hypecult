c395ecd1e8faa5d585555a4f67b34480a5efcb60
jj0han <joaovieirabomfim@gmail.com>
2026-04-10 17:50:30 -0300

    fix(proxy): use string literal for role check

--- proxy.ts
+++ proxy.ts
@@ -1,11 +1,10 @@
 import { NextResponse, type NextRequest } from 'next/server'
-import { UserRole } from './server/db/generated/prisma/enums'
 import { getToken } from 'next-auth/jwt'
  
 // This function can be marked `async` if using `await` inside
 export async function proxy(request: NextRequest) {
   const token = await getToken({ req: request })
-  if (token?.role !== UserRole.admin ) {
+  if (token?.role !== "admin" ) {
     return NextResponse.redirect(new URL('/', request.url))
   }
   return NextResponse.next()
